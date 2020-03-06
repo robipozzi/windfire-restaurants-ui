6836f7b6ccb000c8f448e6ec0a6751fb6ad8c15e
Roberto Pozzi <robertopozzi@Robertos-MBP.lan>
2020-03-06 18:20:55 +0100

    updates

--- src/app/restaurant/restaurant.ts
+++ src/app/restaurant/restaurant.ts
@@ -1,8 +1,9 @@
 export class Restaurant {
+  id: string;
+  name: string;
+  
   constructor(private _id: string, private _name: string) { 
     this.id = _id,
     this.name = _name
   }
-  id: string;
-  name: string;
 }
