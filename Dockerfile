27f6aa0fb97cd4b7e82dc697663ca51ce74769ad
Roberto Pozzi <robertopozzi@Robertos-MBP.lan>
2020-03-29 23:17:11 +0200

    update

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM nginx:1.15.5
-## Remove default nginx website
-RUN rm -rf /usr/share/nginx/html/*
-## From 'builder' stage copy over the artifacts in dist folder to default nginx public folder
-COPY /dist/windfire-restaurants-ui /usr/share/nginx/html
-CMD ["nginx", "-g", "daemon off;"]
--- docker-run.sh
+++
@@ -1,8 +0,0 @@
-source setenv.sh
-
-echo ${cyn}Removing $DOCKER_CONTAINER_NAME Docker container ...${end}
-docker rm -f $DOCKER_CONTAINER_NAME
-echo ${cyn}Docker container removed${end}
-echo
-echo ${cyn}Running Docker container ...${end}
-docker run -it -p 8080:80 --name $DOCKER_CONTAINER_NAME $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION
