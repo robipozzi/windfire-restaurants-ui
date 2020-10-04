a727f77072c067bcf0faeafb4be36a5912b4cfed
Roberto Pozzi <robertopozzi@Robertos-MBP.lan>
2020-10-04 10:42:22 +0200

    update

--- docker-run.sh
+++ docker-run.sh
@@ -1,8 +1,8 @@
 source setenv.sh
 
-echo ${cyn}Removing $CONTAINER_CONTAINER_NAME container ...${end}
-docker rm -f $CONTAINER_CONTAINER_NAME
+echo ${cyn}Removing $CONTAINER_NAME container ...${end}
+docker rm -f $CONTAINER_NAME
 echo ${cyn}Container removed${end}
 echo
-echo ${cyn}Running $CONTAINER_CONTAINER_NAME container ...${end}
-docker run -it -p 8080:8080 --name $CONTAINER_CONTAINER_NAME $CONTAINER_IMAGE_NAME:$CONTAINER_IMAGE_VERSION
+echo ${cyn}Running $CONTAINER_NAME container ...${end}
+docker run -it -p 8080:8080 --name $CONTAINER_NAME $CONTAINER_IMAGE_NAME:$CONTAINER_IMAGE_VERSION
