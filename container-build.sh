6d5c6fcd95ade9b74c3ecdf5814afdc973545f30
Roberto Pozzi <robertopozzi@Robertos-MBP.lan>
2020-11-20 09:18:41 +0100

    update README

--- container-build.sh
+++ container-build.sh
@@ -8,6 +8,7 @@ echo
 echo ${cyn}Removing $CONTAINER_IMAGE_NAME:$CONTAINER_IMAGE_VERSION image ...${end}
 docker rmi -f $CONTAINER_IMAGE_NAME:$CONTAINER_IMAGE_VERSION
 echo ${cyn}Container image removed${end}
+echo
 echo ${cyn}Removing $DOCKER_HUB_ID/$CONTAINER_IMAGE_NAME:$CONTAINER_IMAGE_VERSION image ...${end}
 docker rmi -f $DOCKER_HUB_ID/$CONTAINER_IMAGE_NAME:$CONTAINER_IMAGE_VERSION
 echo ${cyn}Container image removed${end}
