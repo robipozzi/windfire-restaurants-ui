e4581ce4027322fe8409451903e8012141992476
Roberto Pozzi <robertopozzi@Robertos-MBP.lan>
2020-12-04 09:31:08 +0100

    update container scripts

--- container-build.sh
+++ container-build.sh
@@ -20,6 +20,11 @@ echo ${cyn}Container image built${end}
 echo
 echo ${cyn}Pushing $CONTAINER_IMAGE_NAME:$CONTAINER_IMAGE_VERSION image to Docker Hub...${end}
 docker tag $CONTAINER_IMAGE_NAME:$CONTAINER_IMAGE_VERSION $DOCKER_HUB_ID/$CONTAINER_IMAGE_NAME:$CONTAINER_IMAGE_VERSION
+echo
+echo ${cyn}Removing $CONTAINER_IMAGE_NAME:$CONTAINER_IMAGE_VERSION image ...${end}
+docker rmi -f $CONTAINER_IMAGE_NAME:$CONTAINER_IMAGE_VERSION
+echo ${cyn}Container image removed${end}
+echo
 docker push $DOCKER_HUB_ID/$CONTAINER_IMAGE_NAME:$CONTAINER_IMAGE_VERSION
 echo ${cyn}Container image pushed to Docker Hub${end}
 echo
--- container-run.sh
+++ container-run.sh
@@ -5,4 +5,6 @@ docker rm -f $CONTAINER_NAME
 echo ${cyn}Container removed${end}
 echo
 echo ${cyn}Running $CONTAINER_NAME container ...${end}
-docker run -it -p 8080:8080 --name $CONTAINER_NAME $DOCKER_HUB_ID/$CONTAINER_IMAGE_NAME:$CONTAINER_IMAGE_VERSION
+CMD_RUN="docker run -it -p 8080:8080 --name $CONTAINER_NAME $DOCKER_HUB_ID/$CONTAINER_IMAGE_NAME:$CONTAINER_IMAGE_VERSION"
+echo ${cyn}Running Docker container with:${end} ${grn}$CMD_RUN${end}
+$CMD_RUN
