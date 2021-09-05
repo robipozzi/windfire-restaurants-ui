b9e17a83931448ddb7950a3fd121c61081cb8240
Roberto Pozzi <r.robipozzi@gmail.com>
2021-09-05 11:30:12 +0200

    fix security alerts

--- src/environments/environment.mockup.ts
+++ src/environments/environment.mockup.ts
@@ -1,13 +1,13 @@
-import { domain, clientId, audience } from '../../auth_config.json';
+//import { domain, clientId, audience } from '../../auth_config.json';
 export const environment = {
   production: false,
   environment: 'mockup',
   mock: true,
   auth: {
-    domain,
-    clientId,
-    redirectUri: window.location.origin,
-    audience
+    "domain": "robipozzi.eu.auth0.com",
+    "clientId": "Gh3X311uWdYBG0xmUcmzB8vsPito52iw",
+    "audience": "windfire-restaurants",
+    "serverUrl": "http://localhost:8082"
   }
   // This strategy for environment configuration is currently **** NOT USED ****
   //restaurantServiceBaseUrl: window["env"]["restaurantServiceBaseUrl"] || "http://prod:9997"
