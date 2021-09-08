9d1498e43135bf218862c81ab49ae96228b6e279
Roberto Pozzi <r.robipozzi@gmail.com>
2021-09-08 15:15:40 +0200

    updates

--- src/environments/environment.mockup.ts
+++ src/environments/environment.mockup.ts
@@ -6,7 +6,7 @@ export const environment = {
     "domain": "robipozzi.eu.auth0.com",
     "clientId": "Gh3X311uWdYBG0xmUcmzB8vsPito52iw",
     "audience": "windfire-restaurants",
-    "serverUrl": "http://localhost:4200"
+    "serverUrl": "http://localhost:4200/"
   }
   // This strategy for environment configuration is currently **** NOT USED ****
   //restaurantServiceBaseUrl: window["env"]["restaurantServiceBaseUrl"] || "http://prod:9997"
